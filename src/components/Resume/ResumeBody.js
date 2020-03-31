60403865b3100a621a1a3bd7b0a50d0d2b0f0579
murilopanosso <murilopanosso@gmail.com>
2020-03-31 23:41:12 +0100

    fix main container

--- src/components/Resume/ResumeBody.js
+++ src/components/Resume/ResumeBody.js
@@ -30,9 +30,11 @@ const ResumeBody = () => {
   const executeScrollToRef = () => scrollToRef(myref);
   return (
     <div>
-      <Card style={{
-        width: '100%', margin: '0 auto', border: '0px', padding: '20px',
-      }}
+      <Card
+        className="card-root"
+        style={{
+          margin: '0 auto', border: '0px', padding: '20px',
+        }}
       >
         <Card.Body>
           <Card.Title>
