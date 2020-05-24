94ec4df2d98edf636390af7d1938029ef9973a1a
murilopanosso <murilopanosso@gmail.com>
2020-05-24 02:36:47 +0100

    fix build

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 export { default as wrapRootElement } from './src/store/ReduxWrapper';
 
 
-export const onRouteUpdate = ({ location, prevLocation }) => {
+export const onRouteUpdate = ({ location }) => {
   if (location.pathname === '/experience'){
       window.location = '/'
   }
